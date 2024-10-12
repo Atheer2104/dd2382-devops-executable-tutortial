197bd5109f60990a55bd36baa417fb71d6e45829
Noelmt <noelt@kth.se>
2024-10-12 11:57:41 +0200

    Fix: cd to folder

--- killercoda-executable-tutortial/intro/background.sh
+++ killercoda-executable-tutortial/intro/background.sh
@@ -1,4 +1,4 @@
 cd ~  # Reset the working directory to home
 clear
 git clone https://github.com/Atheer2104/dd2382-devops-executable-tutortial.git
-cd dd2382-devops-executable-tutortial/backend-api/
+cd dd2382-devops-executable-tutortial/backend-api
