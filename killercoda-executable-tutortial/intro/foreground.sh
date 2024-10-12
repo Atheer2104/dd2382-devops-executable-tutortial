1c56cb633ee2fea7e88ed46c1807f84994d0678a
Atheer <atheerwaled66@hotmail.se>
2024-10-12 12:19:33 +0200

    chore: change background to foreground

--- killercoda-executable-tutortial/intro/foreground.sh
+++ killercoda-executable-tutortial/intro/foreground.sh
@@ -1,3 +1,7 @@
+ECHO "Cloning Code Respository"
+
 cd filesystem/home/ubuntu/
 git clone https://github.com/Atheer2104/dd2382-devops-executable-tutortial.git
 cd dd2382-devops-executable-tutortial/backend-api/
+
+ECHO "Done"
