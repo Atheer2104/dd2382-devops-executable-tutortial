53e13f065473df373ab2effc69a83e2840ceb221
Atheer <atheerwaled66@hotmail.se>
2024-10-13 11:10:22 +0200

    update python in foreground intro script

--- killercoda-executable-tutortial/intro/foreground.sh
+++ killercoda-executable-tutortial/intro/foreground.sh
@@ -1,8 +1,16 @@
+# cloning code repository
 echo "Cloning Code Respository"
 
 cd filesystem/home/ubuntu/
 git clone https://github.com/Atheer2104/dd2382-devops-executable-tutortial.git
 cd dd2382-devops-executable-tutortial/backend-api/
+
+# install python 3.12
+sudo apt update
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
+sudo apt install python3.12
 
 # install psql
 sudo apt-get install -y postgresql-client
