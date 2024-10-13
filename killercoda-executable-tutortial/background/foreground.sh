79cf78c98729e3694c79f065a93faedd808f81e1
Atheer <atheerwaled66@hotmail.se>
2024-10-13 16:24:19 +0200

    next10

--- killercoda-executable-tutortial/background/foreground.sh
+++ killercoda-executable-tutortial/background/foreground.sh
@@ -2,4 +2,5 @@ echo "Doing setup for you"
 while [ ! -f /tmp/finished ]; do sleep 1; done
 
 cd filesystem/home/ubuntu/dd2382-devops-executable-tutortial/backend-api/
+source env/bin/activate
 echo DONE
