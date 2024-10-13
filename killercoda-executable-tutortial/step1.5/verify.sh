e4f3c3352a6bbdb9533da12c5c8b50aa7f5b9221
Atheer <atheerwaled66@hotmail.se>
2024-10-13 16:38:44 +0200

    chore: add back verify for step 1.5

--- killercoda-executable-tutortial/step1.5/verify.sh
+++ killercoda-executable-tutortial/step1.5/verify.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker ps --filter 'name=postgres' --format '{{.ID}}' | grep ""
 docker ps --filter 'name=redis' --format '{{.ID}}' | grep ""
 
@@ -5,3 +7,7 @@ docker ps --filter 'name=redis' --format '{{.ID}}' | grep ""
 curl http://localhost:8000/api
 curl http://localhost:8000/api/today
 curl http://localhost:8000/api/random
+
+if lsof -i:8000 > /dev/null; then
+  kill $(lsof -t -i:8000)
+fi
