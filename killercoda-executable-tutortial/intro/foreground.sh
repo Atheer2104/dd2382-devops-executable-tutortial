4d3d2adfef8c4a91363bdca4cd47eb4993cfb9d5
Atheer <atheerwaled66@hotmail.se>
2024-10-13 13:16:36 +0200

    remove installing python 3.12

--- killercoda-executable-tutortial/intro/foreground.sh
+++ killercoda-executable-tutortial/intro/foreground.sh
@@ -6,10 +6,10 @@ git clone https://github.com/Atheer2104/dd2382-devops-executable-tutortial.git
 cd dd2382-devops-executable-tutortial/backend-api/
 
 # install python 3.12
-sudo apt update
-sudo add-apt-repository --yes ppa:deadsnakes/ppa
-sudo apt update
+#sudo apt update
+#sudo add-apt-repository --yes ppa:deadsnakes/ppa
+#sudo apt update
 
-sudo apt-get install -y python3.12 
+# sudo apt-get install -y python3.12 
  
 sudo apt-get install -y postgresql-client && export DATABASE_URL=postgres://postgres:password@localhost:5432/users && echo "Done"
