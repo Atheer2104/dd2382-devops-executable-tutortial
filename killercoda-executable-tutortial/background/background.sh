aca0eab0bedfa2142d59030b89ed5c9c129fdb70
Atheer <atheerwaled66@hotmail.se>
2024-10-13 15:34:32 +0200

    chore: background testiing

--- killercoda-executable-tutortial/background/background.sh
+++ killercoda-executable-tutortial/background/background.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# echo starting...
+# cloning code repository
 
-# # cloning code repository
-# echo "Cloning Code Respository"
-
-# cd filesystem/home/ubuntu/
+cd filesystem/home/ubuntu/
 # git clone https://github.com/Atheer2104/dd2382-devops-executable-tutortial.git
 # #cd dd2382-devops-executable-tutortial/backend-api/
 
 # pip install prisma && prisma generate && sudo apt update && sudo add-apt-repository --yes ppa:deadsnakes/ppa && sudo apt update && sudo apt-get install -y python3.12 && curl https://bootstrap.pypa.io/get-pip.py | sudo python3.12 && python3.12 -m pip install virtualenv && python3.12 -m virtualenv env && source env/bin/activate && sudo apt-get install -y postgresql-client && export DATABASE_URL=postgres://postgres:password@localhost:5432/users && touch /tmp/finished
-
-set -x # to test stderr output in /var/log/killercoda
-
-echo starting... # to test stdout output in /var/log/killercoda
-
-sleep 5 # some long running background task
 
 touch /tmp/finished
