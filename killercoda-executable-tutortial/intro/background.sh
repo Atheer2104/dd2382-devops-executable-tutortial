914d30b4793d74e94d270bf7af59d6bb03153a4b
Atheer <atheerwaled66@hotmail.se>
2024-10-13 15:59:35 +0200

    next9

--- killercoda-executable-tutortial/intro/background.sh
+++ killercoda-executable-tutortial/intro/background.sh
@@ -10,5 +10,5 @@ cd filesystem/home/ubuntu/
 git clone https://github.com/Atheer2104/dd2382-devops-executable-tutortial.git
 cd dd2382-devops-executable-tutortial/backend-api/
 
-sudo apt update && sudo add-apt-repository --yes ppa:deadsnakes/ppa && sudo apt update && sudo apt-get install -y python3.12 && curl https://bootstrap.pypa.io/get-pip.py | sudo python3.12  && python3.12 -m pip install virtualenv && python3.12 -m virtualenv env && source env/bin/activate && sudo apt-get install -y postgresql-client && export DATABASE_URL=postgres://postgres:password@localhost:5432/users && echo done > /tmp/finished 
+pip install prisma && prisma generate && sudo apt update && sudo add-apt-repository --yes ppa:deadsnakes/ppa && sudo apt update && sudo apt-get install -y python3.12 && curl https://bootstrap.pypa.io/get-pip.py | sudo python3.12  && python3.12 -m pip install virtualenv && python3.12 -m virtualenv env && source env/bin/activate && sudo apt-get install -y postgresql-client && export DATABASE_URL=postgres://postgres:password@localhost:5432/users && echo done > /tmp/finished 
 
