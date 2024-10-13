226f2d6c14dd95dc2cc062a82a3779fbb969bac2
Atheer <atheerwaled66@hotmail.se>
2024-10-13 13:07:00 +0200

    testing with foreground intro step

--- killercoda-executable-tutortial/intro/foreground.sh
+++ killercoda-executable-tutortial/intro/foreground.sh
@@ -10,7 +10,4 @@ sudo apt update
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
 sudo apt update
 
-sudo apt-get install -y python3.12 && curl https://bootstrap.pypa.io/get-pip.py | sudo python3.12 
-
-
-sudo apt-get install -y postgresql-client && export DATABASE_URL=postgres://postgres:password@localhost:5432/users && echo "Done"
+prisma generate && sudo apt-get install -y python3.12 && curl https://bootstrap.pypa.io/get-pip.py | sudo python3.12 && sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1 && sudo apt-get install -y postgresql-client && export DATABASE_URL=postgres://postgres:password@localhost:5432/users && echo "Done"
